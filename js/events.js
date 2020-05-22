27aa9692ca403d95caaa2598e9e7943e0d8e2440
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-22 13:19:47 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -38,7 +38,7 @@ function submitIt (){
   
   $("form").on("submit", function() {
   if ($( "input:first" ).val() === "correct") {
-    alert('your form is going to be submitted now');
+    alert('Your form is going to be submitted now');
     return;
   }
 
